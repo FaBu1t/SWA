fba57361d981cc57e04c99558e20ec4896421b22
fbuitkam <fabian.buitkamp@hs-osnabrueck.de>
2021-10-26 21:11:11 +0200

    startcontrol

--- swa/src/main/java/de/hsos/swa/ssa/suchen/ui/view/SuchenStartControl.java
+++ swa/src/main/java/de/hsos/swa/ssa/suchen/ui/view/SuchenStartControl.java
@@ -1,5 +1,12 @@
 package de.hsos.swa.ssa.suchen.ui.view;
 
+import de.hsos.swa.ssa.suchen.al.HoleWarenkorb;
+
 public class SuchenStartControl {
+    HoleWarenkorb hWarenkorb;
+    SuchenStartView sView;
+    SuchControl sControl;
+    PruefControl pControl;
+    AuswahlControl aControl;
 
 }
