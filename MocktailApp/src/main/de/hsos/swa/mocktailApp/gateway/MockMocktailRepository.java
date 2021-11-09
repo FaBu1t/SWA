7b7a9760929989580d86e989f808c72777c2fffa
Joana Wegener <joana.wegener98@gmail.com>
2021-11-09 15:37:07 +0100

    fertig mit .entity und .gateway

--- MocktailApp/src/main/de/hsos/swa/mocktailApp/gateway/MockMocktailRepository.java
+++ MocktailApp/src/main/de/hsos/swa/mocktailApp/gateway/MockMocktailRepository.java
@@ -1,36 +1,42 @@
 package de.hsos.swa.mocktailApp.gateway;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import de.hsos.swa.mocktailApp.entity.Mocktail;
 
-public class MockMocktailRepository implements MocktailRepository{
-    private List<Mocktail> mocktails = new ArrayList<Mocktail>();
-    
+public class MockMocktailRepository implements MocktailRepository {
+    private HashMap<Integer, Mocktail> mocktails = new HashMap<Integer, Mocktail>();
 
     @Override
     public Mocktail get(int id) {
-        // TODO Auto-generated method stub
-        return null;
+        if (mocktails.containsKey(id)) {
+            return mocktails.get(id);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public boolean create(int id, String name, String[] zutaten, String autor) {
-        // TODO Auto-generated method stub
-        return false;
+        Mocktail mock = new Mocktail(id, name, zutaten, autor);
+        mocktails.putIfAbsent(id, mock);
+        return true;
     }
 
     @Override
     public boolean update(int id, String name, String[] zutaten, String autor) {
-        // TODO Auto-generated method stub
-        return false;
+        Mocktail mock = new Mocktail(id, name, zutaten, autor);
+        mocktails.put(id, mock);
+        return true;
     }
 
     @Override
     public boolean delete(int id) {
-        // TODO Auto-generated method stub
+        if (mocktails.containsKey(id)) {
+            mocktails.remove(id);
+            return true;
+        }
         return false;
     }
-    
+
 }
