2bb60e289e9e022c94e3819fdb003c3d6bca2618
fbuitkam <fabian.buitkamp@hs-osnabrueck.de>
2021-11-09 11:08:24 +0100

    Reset branch for new

--- swa/src/main/java/de/hsos/swa/ssa/suchen/acl/WarenkorbFuerSuche.java
+++
@@ -1,9 +0,0 @@
-package de.hsos.swa.ssa.suchen.acl;
-
-public interface WarenkorbFuerSuche {
-
-    public void wareHinzufuegen(WareDTO ware);
-
-    public long gebeWarenkorbnummer();
-
-}
--- swa/src/main/java/de/hsos/swa/ssa/suchen/al/SucheWare.java
+++
@@ -1,9 +0,0 @@
-package de.hsos.swa.ssa.suchen.al;
-
-import de.hsos.swa.ssa.suchen.bl.Ware;
-
-public interface SucheWare {
-    public Ware[] sucheWare(String warenname);
-
-    public Ware sucheWare(long warennummer);
-}
