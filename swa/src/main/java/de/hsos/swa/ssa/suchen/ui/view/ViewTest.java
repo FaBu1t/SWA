a2a7b2629508f4dcf6733f8047ac517270721728
Joana Wegener <joana.wegener98@gmail.com>
2021-11-29 16:14:10 +0100

    Aufgabe 5

--- swa/src/main/java/de/hsos/swa/ssa/suchen/ui/view/ViewTest.java
+++
@@ -1,9 +0,0 @@
-package de.hsos.swa.ssa.suchen.ui.view;
-
-public class ViewTest {
-    static SuchenStartControl test;
-
-    public static void main(String[] args) {
-        test = new SuchenStartControl();
-    }
-}
