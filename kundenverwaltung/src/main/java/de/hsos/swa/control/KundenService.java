ee7ffcda1d089c60519ae1e07743e840bd436cc1
Joana Wegener <joana.wegener98@gmail.com>
2021-12-02 17:08:09 +0100

    Methoden angelegt in Service

--- kundenverwaltung/src/main/java/de/hsos/swa/control/KundenService.java
+++ kundenverwaltung/src/main/java/de/hsos/swa/control/KundenService.java
@@ -1,5 +1,41 @@
 package de.hsos.swa.control;
 
+import java.util.Collection;
+
+import de.hsos.swa.entity.Adresse;
+import de.hsos.swa.entity.Kunde;
+
 public class KundenService {
-    
+
+    public void kundenAnlegen(String name) {
+
+    }
+
+    public Collection<Kunde> kundenAbfragen() {
+        return null;
+    }
+
+    public Kunde kundeAbfragen(long kundennr) {
+        return null;
+    }
+
+    public boolean kundeLoeschen(long kundennr) {
+        return false;
+    }
+
+    public void adresseAnlegen(long kundennr, Adresse adr) {
+
+    }
+
+    public void adresseAendern(long kundennr, Adresse neueAdr) {
+
+    }
+
+    public Adresse adresseAbfragen(long kundennr) {
+        return null;
+    }
+
+    public boolean adresseLoeschen(long kundenr) {
+        return false;
+    }
 }
