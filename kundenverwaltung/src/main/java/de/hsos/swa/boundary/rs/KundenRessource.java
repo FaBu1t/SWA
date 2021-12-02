ad62179c450da6ec68b8f689096f550b0fbab864
fbuitkam <fabian.buitkamp@hs-osnabrueck.de>
2021-12-02 17:10:21 +0100

    implemented Kunden+Adresse

--- kundenverwaltung/src/main/java/de/hsos/swa/boundary/rs/KundenRessource.java
+++ kundenverwaltung/src/main/java/de/hsos/swa/boundary/rs/KundenRessource.java
@@ -1,5 +1,21 @@
 package de.hsos.swa.boundary.rs;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import de.hsos.swa.control.KundenService;
+
+
+
+@Path("/kundenvw")
+@Produces("application/json")
+@Consumes("application/json")
 public class KundenRessource {
+
+    @Inject
+    KundenService kundenService;
+
     
 }
--- kundenverwaltung/src/main/java/de/hsos/swa/entity/Kunde.java
+++ kundenverwaltung/src/main/java/de/hsos/swa/entity/Kunde.java
@@ -1,5 +1,28 @@
 package de.hsos.swa.entity;
 
 public class Kunde {
+    private String vorname;
+    private String nachname;
+    private Adresse adresse;
+
+
     
+    public Adresse getAdresse() {
+        return adresse;
+    }
+    public String getVorname() {
+        return vorname;
+    }
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+    public String getNachname() {
+        return nachname;
+    }
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
 }
