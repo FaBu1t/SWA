b884d49c68e9681c04c18d59d277292d4c4f8545
Joana Wegener <joana.wegener98@gmail.com>
2021-12-02 18:02:16 +0100

    Pathes

--- kundenverwaltung/src/main/java/de/hsos/swa/boundary/rs/KundenRessource.java
+++ kundenverwaltung/src/main/java/de/hsos/swa/boundary/rs/KundenRessource.java
@@ -19,8 +19,6 @@ import de.hsos.swa.entity.Adresse;
 import de.hsos.swa.entity.Kunde;
 import de.hsos.swa.control.KundenService;
 
-
-
 @Path("/kundenvw")
 @Produces("application/json")
 @Consumes("application/json")
@@ -29,38 +27,37 @@ public class KundenRessource {
     @Inject
     KundenService kundenService;
 
-
     @GET
     @Path("/kunden")
-    public Response alleKunden(){
-       Collection<Kunde> kunden = kundenService.kundenAbfragen();
-       if (!kunden.isEmpty()){
-           return Response.ok().entity(kunden).build();
-       }
+    public Response alleKunden() {
+        Collection<Kunde> kunden = kundenService.kundenAbfragen();
+        if (!kunden.isEmpty()) {
+            return Response.ok().entity(kunden).build();
+        }
         return Response.noContent().build();
     }
 
     @GET
     @Path("/kunden/{id}")
-    public Response sucheKundeNachId(@PathParam Long id){
+    public Response sucheKundeNachId(@PathParam Long id) {
         Kunde kunde = kundenService.kundeAbfragen(id);
-        if (kunde==null){
+        if (kunde == null) {
             return Response.noContent().build();
         }
         return Response.ok().entity(kunde).build();
     }
 
     @PUT
-    @Path("/neuerkunde/{vorname}/{nachname}")
-    public Response neuerKunde(@PathParam String vorname,@PathParam String nachname){
+    @Path("/kunden/{vorname}/{nachname}")
+    public Response neuerKunde(@PathParam String vorname, @PathParam String nachname) {
         this.kundenService.kundenAnlegen(vorname, nachname);
         return Response.ok().build();
     }
 
     @DELETE
-    public Response loescheKunde(long kundennr){
-        boolean geloescht=this.kundenService.kundeLoeschen(kundennr);
-        if(geloescht){
+    public Response loescheKunde(long kundennr) {
+        boolean geloescht = this.kundenService.kundeLoeschen(kundennr);
+        if (geloescht) {
             return Response.ok().build();
         }
         return Response.status(Status.BAD_REQUEST).build();
@@ -68,9 +65,9 @@ public class KundenRessource {
 
     @GET
     @Path("/adresse/{id}")
-    public Response sucheAdresseNachId(@PathParam long id ){
-        Adresse adresse=this.kundenService.adresseAbfragen(id);
-        if(adresse==null){
+    public Response sucheAdresseNachId(@PathParam long id) {
+        Adresse adresse = this.kundenService.adresseAbfragen(id);
+        if (adresse == null) {
             return Response.noContent().build();
         }
         return Response.ok().entity(adresse).build();
@@ -79,15 +76,16 @@ public class KundenRessource {
 
     @PUT
     @Path("/adresse/{kundennr}")
-    public Response neueAdresse(@PathParam long kundennr, Adresse adresse){
+    public Response neueAdresse(@PathParam long kundennr, Adresse adresse) {
         this.kundenService.adresseAnlegen(kundennr, adresse);
         return Response.noContent().build();
     }
 
     @DELETE
-    public Response loescheAdresse(long kundennr){
-        boolean geloescht=this.kundenService.adresseLoeschen(kundennr);
-        if(geloescht){
+    @Path("/adresse")
+    public Response loescheAdresse(long kundennr) {
+        boolean geloescht = this.kundenService.adresseLoeschen(kundennr);
+        if (geloescht) {
             return Response.ok().build();
         }
         return Response.status(Status.BAD_REQUEST).build();
@@ -95,14 +93,11 @@ public class KundenRessource {
 
     @POST
     @Path("/adresse/{kundennr}")
-    public Response aendereAdresse(@PathParam long kundennr,Adresse adresse){
+    public Response aendereAdresse(@PathParam long kundennr, Adresse adresse) {
         this.kundenService.adresseAendern(kundennr, adresse);
-        
-            return Response.ok().build();
-        
+
+        return Response.ok().build();
+
     }
-    
 
-
-    
 }
