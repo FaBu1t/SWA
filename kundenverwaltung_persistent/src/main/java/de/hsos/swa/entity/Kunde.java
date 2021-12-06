b6963cc9de6868f50cf588d34cb86044a783a0eb
Joana Wegener <joana.wegener98@gmail.com>
2021-12-06 16:09:09 +0100

    Aufgabe 6.2

---
+++ kundenverwaltung_persistent/src/main/java/de/hsos/swa/entity/Kunde.java
@@ -0,0 +1,34 @@
+package de.hsos.swa.entity;
+
+import javax.enterprise.context.Dependent;
+
+public class Kunde {
+    private String vorname;
+    private String nachname;
+    private Adresse adresse;
+
+
+    
+    public Kunde(String vorname, String nachname) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+    }
+    public Adresse getAdresse() {
+        return adresse;
+    }
+    public String getVorname() {
+        return vorname;
+    }
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+    public String getNachname() {
+        return nachname;
+    }
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+}
