745c81c1192605739b67ea46a08def911da9aee8
fbuitkam <fabian.buitkamp@hs-osnabrueck.de>
2021-12-16 17:58:59 +0100

    Added GET alleKunden

--- pizza4me/src/main/java/de/hsos/swa/pizza4me/boundary/rest/KundenRessource.java
+++ pizza4me/src/main/java/de/hsos/swa/pizza4me/boundary/rest/KundenRessource.java
@@ -10,7 +10,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hsos.swa.pizza4me.boundary.dto.KundeDTO;
 import de.hsos.swa.pizza4me.control.KundenService;
+import de.hsos.swa.pizza4me.entity.Kunde;
 
 @Path("/kunde")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -25,10 +30,19 @@ public class KundenRessource {
     @GET
     public Response alleKundenAnzeigen(){
 
+        List<Kunde> kunden=kundenService.alleKundenAnzeigen();
+        if(kunden.isEmpty()){
+            return Response.noContent().build();
+        }
+        List<KundeDTO> kundenDTOs = new ArrayList<>();
+        for(Kunde k:kunden){
+            kundenDTOs.add(KundeDTO.Converter.toKundeDTO(k));
+        }
 
-
-        return Response.noContent().build();
+        return Response.ok(kundenDTOs).build();
         
     }
+
+    
     
 }
